f50f6824cad8c54e96ae3578326763a93f290726
nkmelndz <nkmelndz@gmail.com>
2025-03-30 16:21:54 -0500

    Actualizando SONAR_TOKEN

--- Bank/Bank.Domain.Tests/BankAccountTests.cs
+++ Bank/Bank.Domain.Tests/BankAccountTests.cs
@@ -8,14 +8,14 @@ namespace Bank.Domain.Tests
         [Test]
         public void Debit_WithValidAmount_UpdatesBalance()
         {
-            //Arrange
+            // Arrange
             double beginningBalance = 11.99;
             double debitAmount = 4.55;
             double expected = 7.44;
             BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
-            //Act
+            // Act
             account.Debit(debitAmount);
-            //Assert
+            // Assert
             double actual = account.Balance;
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
